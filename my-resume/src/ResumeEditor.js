8fba6824a7263a7265cfca8657a872f6144692b9
641290900@qq.com <641290900@qq.com>
2019-03-20 23:43:16 +0800

    tab切换

--- my-resume/src/ResumeEditor.js
+++ my-resume/src/ResumeEditor.js
@@ -1,8 +1,44 @@
 import React, { Component } from 'react';
+import './ResumeEditor.scss'
 class ResumeEditor extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            selected : 'introduction',
+            lists: ['introduction', 'education', 'project', 'award', 'internship','others'],
+            introduction : ['姓名','性别','年龄','出生日期','联系方式'],
+            education : ['本科','研究生'],
+            project : ['项目一','项目二','项目三'],
+            award: ['奖励一','奖励二','奖励三'],
+            internship: ['实习一','实习二'],
+            others :['其他']
+        }
+    }
     render(){
+        const listItems = this.state.lists.map((item,index) =>
+            <li key = {index} 
+                className = {this.state.selected === item ? 'active' :''}
+                onClick = {()=>{this.setState({selected:item})}}>
+                {item}
+            </li>
+        );
+        const panelItems = this.state.lists.map((item,index) =>
+            <li key = {index} 
+                className = {this.state.selected === item ? 'show' :'hide'}>
+                {this.state[item]}
+            </li>
+        );
         return(
-            <div id = 'resumeEditor'>I'm ResumeEditor</div>
+            <div id = 'resumeEditor'>
+            <nav>
+                <ol>
+                    {listItems}
+                </ol>
+            </nav>
+            <ol className = 'panels'>
+                {panelItems}
+            </ol>
+            </div>
         )
     }
 }
--- my-resume/src/Topbar.js
+++ my-resume/src/Topbar.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import './Topbar.scss'
 class Topbar extends Component {
     render(){
         return(
             <div id = 'topbar'>
                 <div className="wrapper">
                     <span className="logo">Resumer</span>
+                    <div className = 'action'>
+                        <button className = 'primary'>预览</button>
+                        <button>保存</button>
+                    </div>
                 </div>
             </div>
         )
